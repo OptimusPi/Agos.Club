fc5d83fc7bdd7f04b36f5899c8e0f888f962523d
Ryan <6423385+rhampt@users.noreply.github.com>
2021-01-09 14:37:44 -0500

    Added four mobs

--- src/pages/Mobs.js
+++ src/pages/Mobs.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Typography, Box } from '@material-ui/core';
 
+import royalGuardUri from '../assets/mob/royal_guard.png';
+import desertSweeperUri from '../assets/mob/desert_sweeper.png';
+import desertRangerUri from '../assets/mob/desert_ranger.png';
+import soldierUri from '../assets/mob/soldier.png';
+
 export default function Mobs() {
   return (
     <>
@@ -8,6 +13,10 @@ export default function Mobs() {
         <Typography variant="h4" component="h1" gutterBottom>
           Yeah... I need images.
         </Typography>
+        <img src={royalGuardUri} height="75" alt="Royal Guard" />
+        <img src={desertRangerUri} height="75" alt="Desert Ranger" />
+        <img src={desertSweeperUri} height="75" alt="Desert Sweeper" />
+        <img src={soldierUri} height="75" alt="Solder" />
       </Box>
     </>
   );
